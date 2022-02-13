27fe0e7879145d162219f4130b616daf00c019eb
HussamCheema <coms.copp@gmail.com>
2022-02-13 21:51:00 +0500

    Bert Sentiment Analysis

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bert-sentiment
 Sentiment Analysis using BERT
+
+-> Download "bert_base_uncased" model from hugging face or from here: https://www.kaggle.com/abhishek/bert-base-uncased
+-> Put this model in input folder
+-> This project also include Flask REST API
---
+++ src/config.py
@@ -0,0 +1,15 @@
+import transformers
+
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+EPOCHS = 1
+NUM_WORKERS = 4
+BERT_PATH = "../input/bert_base_uncased/"
+MODEL_PATH = "model.bin"
+TRAINING_FILE = "../input/imdb.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BERT_PATH, 
+    do_lower_case=True
+)
+
